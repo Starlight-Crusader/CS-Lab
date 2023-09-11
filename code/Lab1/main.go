dad4ce638d91929969e64878d27ebd33926456e1
AKalamaghin <arteom.kalamaghin@isa.utm.md>
2023-09-11 18:18:39 +0300

    Lab1 finished

---
+++ code/Lab1/main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func input(option *int, k1 *int, k2 *string, line *string) {
+	fmt.Print("What operation do you want to perform? 1. Enc / 2. Dec - ")
+	_, _ = fmt.Scanln(option)
+
+	fmt.Print("What is the key1 value? - ")
+	_, _ = fmt.Scanln(k1)
+
+	in := bufio.NewReader(os.Stdin)
+
+	fmt.Print("What is the key2 value? - ")
+	*k2, _ = in.ReadString('\n')
+
+	fmt.Print("What is the message/cryptogram? ('A' -> 'Z', 'a' -> 'z') : ")
+	*line, _ = in.ReadString('\n')
+}
+
+func validate(line string, alphabet []int32) bool {
+	if len(line) == 0 {
+		return true
+	}
+
+	var verdict bool
+
+	for _, charL := range line {
+		verdict = false
+
+		for _, charA := range alphabet {
+			if charL == charA {
+				verdict = true
+				break
+			}
+		}
+
+		if verdict == false {
+			break
+		}
+	}
+
+	return verdict
+}
+
+func format(line string) string {
+	var newLine string
+
+	for _, char := range line {
+		if char != ' ' {
+			newLine += string(char)
+		}
+	}
+
+	newLine = newLine[0 : len(newLine)-1]
+
+	return newLine
+}
+
+func encdec(line string, key int, operation int, alphabet []int32) string {
+	var newMsg string
+
+	for _, charL := range line {
+		var newCharIndex int
+
+		for index, charA := range alphabet {
+			if charL == charA {
+				if operation == 1 {
+					newCharIndex = (index + key) % len(alphabet)
+				} else {
+					newCharIndex = (index - key) % len(alphabet)
+					if newCharIndex < 0 {
+						newCharIndex = len(alphabet) + newCharIndex
+					}
+				}
+				break
+			}
+		}
+
+		newMsg += string(alphabet[newCharIndex])
+	}
+
+	return newMsg
+}
+
+func permuteAlphabet(key2 string, alphabet []int32) []int32 {
+	var newAlphabet []int32
+
+	for _, char := range key2 {
+		newAlphabet = append(newAlphabet, char)
+	}
+
+	for _, char := range alphabet {
+		newAlphabet = append(newAlphabet, char)
+	}
+
+	edited := true
+
+	for edited {
+		edited = false
+
+		for i, charI := range newAlphabet[:len(newAlphabet)-1] {
+			for j, charJ := range newAlphabet[i+1:] {
+				if charI == charJ {
+					newAlphabet = append(newAlphabet[:j+i+1], newAlphabet[j+i+2:]...)
+					edited = true
+
+					break
+				}
+			}
+
+			if edited {
+				break
+			}
+		}
+	}
+
+	return newAlphabet
+}
+
+func main() {
+	var alphabet []int32
+
+	for char := 'A'; char <= 'Z'; char++ {
+		alphabet = append(alphabet, char)
+	}
+
+	var option, k1 int
+	var k2, line string
+
+	input(&option, &k1, &k2, &line)
+
+	line = format(strings.ToUpper(line))
+
+	if len(k2) > 0 {
+		k2 = format(strings.ToUpper(k2))
+	}
+
+	if !validate(line, alphabet) || !validate(k2, alphabet) {
+		fmt.Println("==================================")
+		fmt.Println("ERROR: Unknown character detected!")
+		fmt.Println("==================================")
+
+		return
+	}
+
+	if len(k2) > 0 {
+		alphabet = permuteAlphabet(k2, alphabet)
+		fmt.Println(string(alphabet))
+	}
+
+	fmt.Println("----------------------------------")
+	fmt.Println(encdec(line, k1, option, alphabet))
+}
